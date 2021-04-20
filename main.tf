97ac018bd6b645af3a837ed2f28e223de4d91d8c
itom-qe <79703359+itom-qe@users.noreply.github.com>
2021-04-20 19:49:38 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,77 @@
+variable "subscriptionId" {}
+variable "clientId" {}
+variable "clientSecret" {}
+variable "tenantId" {}
+
+provider "azurerm" {
+  subscription_id = "${var.subscriptionId}"
+  client_id       = "${var.clientId}"
+  client_secret   = "${var.clientSecret}"
+  tenant_id       = "${var.tenantId}"
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-resources"
+  location = "${var.region}"
+  tags = "${local.common_tags}"
+}
+
+resource "azurerm_app_service_plan" "main" {
+  name                = "${var.prefix}-asp"
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+  tags = "${local.common_tags}"
+}
+
+resource "azurerm_app_service" "main" {
+  name                = "${var.prefix}-appservice"
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.main.id}"
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    remote_debugging_enabled = true
+    remote_debugging_version = "VS2015"
+  }
+  tags = "${local.common_tags}"
+}
+
+locals {
+  # Commmon tags to be assigned to all resources
+  common_tags = "${map(
+   " User", "${var.user}",
+   " CostCenter", "${var.cost_center}"
+ )}"
+}
+
+
+variable "prefix" {
+  description = "The prefix used for all resources in this example"
+  default = "npr001"
+}
+
+variable "region" {}
+
+
+variable "user" {
+  description = "User requesting the resources"
+}
+
+variable "cost_center" {
+  description = "Cost Center of the User requesting the resources"
+}
+
+output "app_service_name" {
+  value = "${azurerm_app_service.main.name}"
+}
+
+output "app_service_default_hostname" {
+  value = "https://${azurerm_app_service.main.default_site_hostname}"
+}
+
