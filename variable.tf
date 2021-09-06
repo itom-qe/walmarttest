333ab1085733fdc2b3f764396e00071c9a88d9e8
itom-qe <79703359+itom-qe@users.noreply.github.com>
2021-09-06 16:00:29 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,17 @@
+variable "prefix" {
+  description = "The prefix used for all resources in this example"
+  default = "npr001"
+}
+
+variable "region" {}
+
+
+variable "user" {
+  description = "User requesting the resources"
+  default = "admin"
+}
+
+variable "cost_center" {
+  description = "Cost Center of the User requesting the resources"
+  default = "admin"
+}
