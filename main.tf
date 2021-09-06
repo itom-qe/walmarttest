b70427b605b5765f33a7398cc833b87b25e322e4
itom-qe <79703359+itom-qe@users.noreply.github.com>
2021-09-06 16:27:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-provider "azurerm" {
-  subscription_id = "${var.subscriptionId}"
-  client_id       = "${var.clientId}"
-  client_secret   = "${var.clientSecret}"
-  tenant_id       = "${var.tenantId}"
-  version = "=1.44.0"
-}
-
 
 resource "azurerm_resource_group" "main" {
   name     = "tfeautorgdnd1"
